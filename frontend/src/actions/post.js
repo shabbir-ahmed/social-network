166a0839cfb27803bef6a47cba54f122a70d41ad
shabbir-ahmed <9017121+shabbir-ahmed@users.noreply.github.com>
2022-03-21 15:11:04 +0600

    like/unlike/deletepost

--- frontend/src/actions/post.js
+++ frontend/src/actions/post.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { GET_POSTS, POST_ERROR } from "./types";
+import { DELETE_POST, GET_POSTS, POST_ERROR, UPDATE_LIKE } from "./types";
+import { setAlert } from "./alert";
 
 // Get posts
 export const getPosts = () => async (dispatch) => {
@@ -10,6 +11,65 @@ export const getPosts = () => async (dispatch) => {
 			type: GET_POSTS,
 			payload: res.data,
 		});
+	} catch (err) {
+		dispatch({
+			type: POST_ERROR,
+			payload: {
+				msg: err.response.statusText,
+				status: err.response.status,
+			},
+		});
+	}
+};
+
+// Add like
+export const addLike = (postId) => async (dispatch) => {
+	try {
+		const res = await axios.put(`/api/posts/like/${postId}`);
+
+		dispatch({
+			type: UPDATE_LIKE,
+			payload: { postId, likes: res.data },
+		});
+	} catch (err) {
+		dispatch({
+			type: POST_ERROR,
+			payload: {
+				msg: err.response.statusText,
+				status: err.response.status,
+			},
+		});
+	}
+};
+
+// Remove like
+export const removeLike = (postId) => async (dispatch) => {
+	try {
+		const res = await axios.put(`/api/posts/unlike/${postId}`);
+		dispatch({
+			type: UPDATE_LIKE,
+			payload: { postId, likes: res.data },
+		});
+	} catch (err) {
+		dispatch({
+			type: POST_ERROR,
+			payload: {
+				msg: err.response.statusText,
+				status: err.response.status,
+			},
+		});
+	}
+};
+
+// Delete post
+export const deletePost = (postId) => async (dispatch) => {
+	try {
+		const res = await axios.delete(`/api/posts/${postId}`);
+		dispatch({
+			type: DELETE_POST,
+			payload: postId,
+		});
+		dispatch(setAlert("Post removed successfully!", "success"));
 	} catch (err) {
 		dispatch({
 			type: POST_ERROR,
--- frontend/src/components/posts/PostItem.js
+++ frontend/src/components/posts/PostItem.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import Moment from "react-moment";
+import { addLike, removeLike, deletePost } from "../../actions/post";
 
 const PostItem = ({
 	auth,
 	post: { _id, name, text, avatar, user, likes, comments, date },
+	addLike,
+	removeLike,
+	deletePost,
 }) => {
 	return (
 		<div className="post bg-white p-1 my-1">
@@ -16,21 +21,37 @@ const PostItem = ({
 			</div>
 			<div>
 				<p className="my-1">{text}</p>
-				<p className="post-date">Posted on 04/16/2019</p>
-				<button type="button" className="btn btn-light">
-					<i className="fas fa-thumbs-up"></i>
+				<p className="post-date">
+					Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
+				</p>
+				<button
+					onClick={() => addLike(_id)}
+					type="button"
+					className="btn btn-light"
+				>
+					<i className="fas fa-thumbs-up"></i>{" "}
 					<span>{likes.length}</span>
 				</button>
-				<button type="button" className="btn btn-light">
+				<button
+					onClick={() => removeLike(_id)}
+					type="button"
+					className="btn btn-light"
+				>
 					<i className="fas fa-thumbs-down"></i>
 				</button>
-				<Link to="/post" className="btn btn-primary">
+				<Link to={`/post/${_id}`} className="btn btn-primary">
 					Discussion{" "}
 					<span className="comment-count">{comments.length}</span>
 				</Link>
-				<button type="button" className="btn btn-danger">
-					<i className="fas fa-times"></i>
-				</button>
+				{!auth.loading && user === auth.user._id && (
+					<button
+						onClick={() => deletePost(_id)}
+						type="button"
+						className="btn btn-danger"
+					>
+						<i className="fas fa-times"></i>
+					</button>
+				)}
 			</div>
 		</div>
 	);
@@ -40,4 +61,6 @@ const mapStateToProps = (state) => ({
 	auth: state.auth,
 });
 
-export default connect(mapStateToProps)(PostItem);
+export default connect(mapStateToProps, { addLike, removeLike, deletePost })(
+	PostItem
+);
